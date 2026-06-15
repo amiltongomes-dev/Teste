6c8e49987389c3985907818162175ec0749da187
AmiltonGomes-Dev <amilton.gomes@prof.facape.br>
2026-06-15 18:59:11 -0300

    inclusao frontend e cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const database = require("./config/database")
 const routes = require("./routes/produtoRoutes")
+const cors = require('cors');
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors());
 
 app.use(routes)
 
